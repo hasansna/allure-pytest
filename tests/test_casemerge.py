6cb3bf401987e73b7dda4f9d462205d6dcbeb2c4
TietoLV <nadezda.jefimova@tieto.com>
2017-09-08 17:40:07 +0300

    simple

--- tests/test_casemerge.py
+++ tests/test_casemerge.py
@@ -14,20 +14,23 @@ def test_smoke(report_for):
 
     extra_run_args = list()
 
-    extra_run_args.extend(['--allure_casemerge', '4'])
+    extra_run_args.extend(['--allure_casemerge', '2'])
 
     report = report_for("""
     import pytest
     import allure
-    @pytest.mark.parametrize("fake_input",  range(1, 11))
-    def test_passed(fake_input):
-    with allure.step("Step #1"):
-        with allure.step("Step #2"):
-            assert 1==1
+    def test_passed1():
+        assert 1==1
+
+    def test_passed2():
+        assert 1==1
+
+    def test_passed3():
+        assert 1==1
     """ , extra_run_args=extra_run_args)
 
     #print len(report.findall('test-cases/test-case'))
     #for test_case in report.findall('test-cases/test-case'):
     #    print test_case.find('name').text
-    assert len(report.findall('test-cases/test-case')) == 3
+    assert len(report.findall('test-cases/test-case')) == 2
 
